c4382cc4f241dff3bba159927aa597087b8066c3
Dorian Sobierański <dorian.sobieranski@gmail.com>
2025-05-13 16:19:29 +0200

    added source.py and audio

--- Makefile
+++ Makefile
@@ -1,2 +1,4 @@
 window:
 	.venv/bin/activate && python app/window.py
+source:
+	.venv/bin/activate && python app/source.py
--- app/source.py
+++ app/source.py
@@ -2,6 +2,7 @@ import pyaudio
 import time
 import math
 import numpy as np
+import librosa
 
 from utils import logger
 
@@ -20,9 +21,39 @@ class Source:
     def callback(self,data,frame_count,time_info,status):
         raise NotImplementedError("source.callback")
     
+SAMPLE_RATE = 22050
+BUFFER_SIZE = 1024
 
 class File(Source):
-    pass
+    
+    def init(self,filename):
+        self.data,_ = librosa.load(filename,sr=SAMPLE_RATE)
+        self.steam = self.audio.open(
+            format=pyaudio.paFloat32,
+            channels=1,
+            rate=SAMPLE_RATE,
+            output=True,
+            frames_per_buffer=BUFFER_SIZE,
+            stream_callback=self.callback)
+        
+    def callback(self,in_data,frame_count, time_info,status):
+        a = self.total
+        b = self.total + BUFFER_SIZE
+        data = self.data[a:b]
+        self.total = b
+        if self.total >= len(self.data):
+            self.complete = True
+        return (data,pyaudio.paContinue)
+    
 
 class Microphone(Source):
     pass
+
+
+
+if __name__ == '__main__':
+    filename = './audio/aphex_test.mp3'
+
+    source = File(filename)
+
+    time.sleep(5)
